1375dba9fb99deacbfda7531a5a434fcd403028a
Eoin Breen <eoin.breen@linkiotech.com>
2016-07-28 21:49:37 +0100

    Migration of site from Google Drive to Github Pages

---
+++ MyFunction.js
@@ -0,0 +1,31 @@
+
+$('#about').click(function () {
+  {
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $('#c').offset().top
+    }, 'slow');
+}});
+
+$('#homemenu').click(function () {
+  {
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $('#home').offset().top
+    }, 'slow');
+}});
+
+$('#ys2014').click(function () {
+  {
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $('#d').offset().top
+    }, 'slow');
+}});
+$('#contact-menu').click(function () {
+  {
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $('#contact').offset().top
+    }, 'slow');
+}});
